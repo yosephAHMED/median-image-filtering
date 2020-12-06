9bec9479f13982cd6025bbb3bd46510809cf90ec
YosephAhmed <yosephahmed@aol.com>
2020-12-06 14:59:39 -0500

    feat: implemented a function that generates n random numbers from 1 to N

--- median-image-filtering/Source.cpp
+++ median-image-filtering/Source.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
+#include <fstream>
+#include <chrono>
+#include <ctime>
+#include <cstdlib>
 
 using namespace std;
 
+void generateRandomInput(int* arr, int n) {
+    for (int i = 0; i < n; i++) {
+        arr[i] = rand() % n + 1;
+    }
+}
+
 int main() {
+
+    ofstream outFile;
+    outFile.open("results.txt");
+
+    // computer internal clock controls the choice of the seed
+    srand(unsigned(time(NULL)));
+
+    int* arr = new int[10];
+    generateRandomInput(arr, 10);
+
 	system("pause");
 	return 0;
 }
