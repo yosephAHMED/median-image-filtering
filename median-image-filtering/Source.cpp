b992d4f4cd0f8f603da78f55398121af8f750d6e
YosephAhmed <yosephahmed@aol.com>
2020-12-06 17:29:33 -0500

    feat: added a new .cpp file that contains the order statistic algorithm. commiting changes before refactoring to fit dynamic array implementation

--- median-image-filtering/Source.cpp
+++ median-image-filtering/Source.cpp
@@ -6,23 +6,26 @@
 
 using namespace std;
 
-void generateRandomInput(int* arr, int n) {
+/* The generateRandomInput() function generates n random 
+numbers in the range of [1..N], which are unordered.
+*/
+void generateRandomInput(int* arr, int N, int n) {
     for (int i = 0; i < n; i++) {
-        arr[i] = rand() % n + 1;
+        arr[i] = rand() % N + 1;
     }
 }
 
-int main() {
-
-    ofstream outFile;
-    outFile.open("results.txt");
-
-    // computer internal clock controls the choice of the seed
-    srand(unsigned(time(NULL)));
-
-    int* arr = new int[10];
-    generateRandomInput(arr, 10);
-
-	system("pause");
-	return 0;
-}
+//int main() {
+//
+//    ofstream outFile;
+//    outFile.open("results.txt");
+//
+//    // computer internal clock controls the choice of the seed
+//    srand(unsigned(time(NULL)));
+//
+//    int* arr = new int[10];
+//    generateRandomInput(arr, 10);
+//
+//	system("pause");
+//	return 0;
+//}
