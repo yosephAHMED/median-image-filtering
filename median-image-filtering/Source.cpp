10867c78986e72f749f58cd3723f27589b0bffcd
YosephAhmed <yosephahmed@aol.com>
2020-12-07 23:48:46 -0500

    feat: partial median filtering implementation

--- median-image-filtering/Source.cpp
+++ median-image-filtering/Source.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include <iomanip>
 #include <fstream>
+#include <sstream>
 #include <chrono>
 #include <ctime>
 #include <cstdlib>
+
+extern "C" {
+#include "imageio.h"
+}
 
 using namespace std;
 
@@ -16,7 +21,8 @@ double arrAverage(double arr[], int n);
 // OrderStatistic function prototype
 int kthSmallest(int* arr, int l, int r, int k);
 
-int main() {
+void orderStatisticExperiment() {
+    cout << "Running Order Statistic Experiment" << endl;
 
     ofstream outFile;
     outFile.open("results.txt");
@@ -34,7 +40,7 @@ int main() {
 
     // Sample size array
     int n[6] = { 100, 300, 500, 1000, 2000, 4000 };
-    
+
     // array to store random k, ith smallest number
     int* k = new int[5];
 
@@ -49,7 +55,7 @@ int main() {
 
     for (int sampleSizeIndex = 0; sampleSizeIndex < 6; sampleSizeIndex++) {
         outFile << "Sample size: " << n[sampleSizeIndex] << endl;
-        
+
         // obtain 5 random values for k from [1..n]
         generateRandomInput(k, n[sampleSizeIndex], 5);
 
@@ -91,6 +97,117 @@ int main() {
         }
         // write average RT of averages to file
         outFile << setw(57) << "Average RT across Populations: " << arrAverage(averageRT, 3) << " ns" << endl;
+    }
+    outFile.close();
+}
+
+void medianImageFiltering() {
+    int windowSize;
+    int imageArr[2000][2000];
+    int medArr[2000][2000];
+
+    cout << "Median image filtering selected" << endl;
+    cout << "Enter window size: ";
+    cin >> windowSize;
+
+    // (n x n) window
+    int* window = new int[windowSize*windowSize];
+    int numWindowEle = windowSize * windowSize;
+
+    int row = 0, col = 0, numRows = 0, numCols = 0, max = 0;
+
+    ifstream infile("noisyImageFiftyHundred.pgm");
+    stringstream ss;
+    string inputLine = "";
+
+    // First line of .pgm file contains version 
+    getline(infile, inputLine);
+    if (inputLine.compare("P2") != 0) cerr << "Version error" << endl;
+    else cout << "Version : " << inputLine << endl;
+
+    // Continue with a stringstream 
+    ss << infile.rdbuf();
+
+    // Secondline : size of image (cols by rows)
+    // Thirdline : max intensity of a pixel (0-255)
+    ss >> numCols >> numRows >> max;
+
+    //print total number of rows, columns and maximum intensity of image 
+    cout << numCols << " columns and " << numRows << " rows" << endl << "Maximium Intesity "<< max <<endl;  
+
+    //Initialize a new array of same size of image with 0 
+    for (row = 0; row <= numRows; ++row)
+        imageArr[row][0] = 0;
+    for (col = 0; col <= numCols; ++col)
+        imageArr[0][col] = 0;
+
+    // Copying the data from .pgm to imageArr
+    for (row = 1; row <= numRows; ++row)
+    {
+        for (col = 1; col <= numCols; ++col)
+        {
+            //original data store in new array 
+            ss >> imageArr[row][col];
+        }
+    }
+
+    for (int row = 1; row < numRows - windowSize + 1; ++row)
+    {
+        for (int col = 1; col < numCols - windowSize + 1; ++col)
+        {
+            // Copy window
+            for (int y = 0; y < windowSize; ++y)
+            {
+                for (int x = 0; x < windowSize; ++x)
+                {
+                    window[y * windowSize + x] = imageArr[row + y][col + x];
+                }
+            }
+
+            // window's "center"
+            int cx = col + windowSize / 2;
+            int cy = row + windowSize / 2;
+
+            // apply order statistic to window array and store the median in the new array 
+            medArr[row][col] = kthSmallest(window, 0, numWindowEle - 1, numWindowEle / 2);
+        }
+    }
+
+    ofstream outfile;
+
+    //new file open to stroe the output image 
+    outfile.open("Medianfilter.pnm");
+    outfile << "P2" << endl;
+    outfile << numCols << " " << numRows << endl;
+    outfile << "255" << endl;
+
+    for (row = 1; row <= numRows; ++row)
+    {
+        for (col = 1; col <= numCols; ++col)
+        {
+            //store resultant pixel values to the output file 
+            outfile << medArr[row][col] << " ";
+        }
+    }
+
+    outfile.close();
+    infile.close();
+}
+
+int main() {
+    int userChoice = -1, windowSize;
+
+    cout << "1: Run order statistic experiment" << endl;
+    cout << "2: Apply median image filtering" << endl;
+    cin >> userChoice;
+
+    switch (userChoice) {
+    case 1:
+        orderStatisticExperiment();
+        break;
+    case 2:
+        medianImageFiltering();
+        break;
     }
 
 	system("pause");
